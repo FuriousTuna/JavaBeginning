75ac524a0d506b4f85ff46d7f21372ee2dc0eb57
Maxim Altunin <maxim12340@gmail.com>
2021-02-13 20:03:32 +0300

    fix FifthTask.

--- src/chapterSix/FifthTask.java
+++ src/chapterSix/FifthTask.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class FifthTask {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число:");
         double a = sc.nextDouble();
+        System.out.println("Введите второе число:");
         double b = sc.nextDouble();
+        System.out.println("Введите знак операции (+, -, *, /):");
         String symbol = sc.next();
 
         switch (symbol) {
@@ -20,7 +23,11 @@ public class FifthTask {
                 System.out.println(a * b);
                 break;
             case "/":
-                System.out.println(a / b);
+                if (b == 0) {
+                    System.out.println("Делить на ноль нельзя!!!");
+                } else {
+                    System.out.println(a / b);
+                }
                 break;
         }
     }
